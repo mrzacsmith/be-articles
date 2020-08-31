2c5795b69ad4ad1f0284a605b9f26359c42a27b4
Zac Smith <zrs3141592@gmail.com>
2020-08-30 21:20:44 -0600

    basic main page styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/layouts/Header.jsx'
+import Navbar from './components/layouts/Navbar.jsx'
+import Footer from './components/layouts/Footer.jsx'
 import './App.css'
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Navbar />
+      <Footer />
     </div>
   )
 }
