9dacd3ccc2d14de5a7b2376485243982b7b31a93
Zac Smith <zrs3141592@gmail.com>
2020-08-30 23:27:57 -0600

    styling

--- client/src/components/Articles.jsx
+++ client/src/components/Articles.jsx
@@ -1,19 +1,37 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Articles = ({ posts }) => {
   return (
-    <div>
-      Welcome to the articles component
+    <ArticleContainer>
       {posts.map((article, key) => (
-        <div>
-          <h2>
-            {article.title} by {article.authorname}
-          </h2>
+        <div className="container">
+          <h2>{article.title} by </h2>
           <p>{article.article}</p>
+          <span className="badge badge-secondary p-2">
+            {article.authorname}
+          </span>
+          <div className="row my-5">
+            <div className="col-sm-2">
+              <a href="" className="btn btn-outline-success">
+                Edit
+              </a>
+            </div>
+            <div className="col-sm-2">
+              <a href="" className="btn btn-outline-danger">
+                Delete
+              </a>
+            </div>
+          </div>
         </div>
       ))}
-    </div>
+    </ArticleContainer>
   )
 }
 
 export default Articles
+
+const ArticleContainer = styled.div`
+  margin: 5rem 0;
+  text-align: left;
+`
