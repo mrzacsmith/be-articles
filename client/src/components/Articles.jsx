d2a174b3e1b7673497ad78a1f1123f9369d2a981
Zac Smith <zrs3141592@gmail.com>
2020-08-31 14:38:29 -0600

    add concurrently

--- client/src/components/Articles.jsx
+++ client/src/components/Articles.jsx
@@ -4,27 +4,31 @@ import styled from 'styled-components'
 const Articles = ({ posts }) => {
   return (
     <ArticleContainer>
-      {posts.map((article, key) => (
-        <div className="container">
-          <h2>{article.title} by </h2>
-          <p>{article.article}</p>
-          <span className="badge badge-secondary p-2">
-            {article.authorname}
-          </span>
-          <div className="row my-5">
-            <div className="col-sm-2">
-              <a href="" className="btn btn-outline-success">
-                Edit
-              </a>
-            </div>
-            <div className="col-sm-2">
-              <a href="" className="btn btn-outline-danger">
-                Delete
-              </a>
+      {!posts.length ? (
+        <h1>Loading ... </h1>
+      ) : (
+        posts.map((article, key) => (
+          <div className="container">
+            <h2>{article.title} by </h2>
+            <p>{article.article}</p>
+            <span className="badge badge-secondary p-2">
+              {article.authorname}
+            </span>
+            <div className="row my-5">
+              <div className="col-sm-2">
+                <a href="" className="btn btn-outline-success">
+                  Edit
+                </a>
+              </div>
+              <div className="col-sm-2">
+                <a href="" className="btn btn-outline-danger">
+                  Delete
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </ArticleContainer>
   )
 }
--- client/src/components/layouts/Navbar.jsx
+++ client/src/components/layouts/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
             </li>
             <li className="nav-item">
               <a className="nav-link" href="#">
-                Link
+                Add Article
               </a>
             </li>
           </ul>
