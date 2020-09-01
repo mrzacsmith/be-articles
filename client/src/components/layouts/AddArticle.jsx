2d18ada34c2068b284647926d471bead3387642f
Zac Smith <zrs3141592@gmail.com>
2020-08-31 22:55:30 -0600

    add new article is successfully posting

--- client/src/components/layouts/AddArticle.jsx
+++ client/src/components/layouts/AddArticle.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import axios from 'axios'
 
 const AddArticle = () => {
+  const [title, setTitle] = useState('')
+  const [article, setArticle] = useState('')
+  const [authorname, setAuthorname] = useState('')
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    const articles = {
+      title,
+      article,
+      authorname,
+    }
+
+    axios
+      .post('http://localhost:5775/articles/add', articles)
+      .then((res) => console.log(res.data))
+      .catch((err) => {
+        console.log('Error: ' + err)
+      })
+  }
+
   return (
     <AddArticleContainer>
       <div className="container">
         <h2>Add a new article!</h2>
-        <form>
+        <form onSubmit={onSubmitHandler} encType="multipart/form-data">
           <div className="form-group">
             <label htmlFor="authorname">Author Name</label>
             <input
               type="text"
+              onChange={(e) => setAuthorname(e.target.value)}
               className="form-control"
               placeholder="author name"
             />
@@ -19,6 +41,7 @@ const AddArticle = () => {
             <label htmlFor="title">Title</label>
             <input
               type="text"
+              onChange={(e) => setTitle(e.target.value)}
               className="form-control"
               id="title"
               placeholder="title"
@@ -26,7 +49,11 @@ const AddArticle = () => {
           </div>
           <div className="form-group">
             <label htmlFor="article">Article</label>
-            <textarea className="form-control" rows="3"></textarea>
+            <textarea
+              onChange={(e) => setArticle(e.target.value)}
+              className="form-control"
+              rows="3"
+            ></textarea>
           </div>
 
           <button type="submit" className="btn btn-primary">
