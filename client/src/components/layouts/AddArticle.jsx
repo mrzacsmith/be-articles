d408881131782c077e53686d021bdbb38889b347
Zac Smith <zrs3141592@gmail.com>
2020-08-31 20:17:17 -0600

    create addArticle form

--- client/src/components/layouts/AddArticle.jsx
+++ client/src/components/layouts/AddArticle.jsx
@@ -1,7 +1,31 @@
 import React from 'react'
 
 const AddArticle = () => {
-  return <div>Hello from Add Article</div>
+  return (
+    <form>
+      <div className="form-group">
+        <label htmlFor="authorname">Author Name</label>
+        <input type="text" className="form-control" placeholder="author name" />
+      </div>
+      <div className="form-group">
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          className="form-control"
+          id="title"
+          placeholder="title"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="article">Article</label>
+        <textarea className="form-control" rows="3"></textarea>
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  )
 }
 
 export default AddArticle
