3b7c51ac036b9b00bfd60063ac6033197f46f328
Zac Smith <zrs3141592@gmail.com>
2020-08-31 22:35:17 -0600

    finish AddArticle

--- client/src/components/layouts/AddArticle.jsx
+++ client/src/components/layouts/AddArticle.jsx
@@ -1,31 +1,61 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const AddArticle = () => {
   return (
-    <form>
-      <div className="form-group">
-        <label htmlFor="authorname">Author Name</label>
-        <input type="text" className="form-control" placeholder="author name" />
-      </div>
-      <div className="form-group">
-        <label htmlFor="title">Title</label>
-        <input
-          type="text"
-          className="form-control"
-          id="title"
-          placeholder="title"
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="article">Article</label>
-        <textarea className="form-control" rows="3"></textarea>
-      </div>
+    <AddArticleContainer>
+      <div className="container">
+        <h2>Add a new article!</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="authorname">Author Name</label>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="author name"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="title">Title</label>
+            <input
+              type="text"
+              className="form-control"
+              id="title"
+              placeholder="title"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="article">Article</label>
+            <textarea className="form-control" rows="3"></textarea>
+          </div>
 
-      <button type="submit" className="btn btn-primary">
-        Submit
-      </button>
-    </form>
+          <button type="submit" className="btn btn-primary">
+            Post Article
+          </button>
+        </form>
+      </div>
+    </AddArticleContainer>
   )
 }
 
 export default AddArticle
+
+const AddArticleContainer = styled.div`
+  margin: 3rem auto;
+  padding: 4rem;
+  width: 40rem;
+
+  h2 {
+    font-weight: 900;
+    color: var(--dark-green);
+  }
+
+  .btn-primary {
+    margin-top: 2rem;
+    background: var(--dark-green);
+    border: none;
+    &:hover {
+      background: var(--light-green);
+    }
+  }
+`
